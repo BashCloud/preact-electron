7e679ed03e3fa32bf7ee4ecb080960511b75a480
Pulkit Aggarwal <pulkit.cloud@gmail.com>
2017-07-23 10:46:12 +0530

    ADDED: Default view and 404 view.

--- app/components/app.js
+++ app/components/app.js
@@ -10,18 +10,15 @@ export default class App extends Component {
 	 *	@param {Object} event		"change" event from [preact-router](http://git.io/preact-router)
 	 *	@param {string} event.url	The newly routed URL
 	 */
-	handleRoute = e => {
-		this.currentUrl = e.url;
-	};
-
 	render() {
 		return (
 			<div id="app">
-				  <Header />
-				 <Router onChange={this.handleRoute}>
+				<Header />
+				<Router>
 					<Home path="/" />
 					<Profile path="/profile/" user="me" />
 					<Profile path="/profile/:user" />
+					<Home type="404" default />
 				</Router>   
 			</div>
 		);
