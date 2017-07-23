efa090f0b506a34aaad82b80672be4fcf2aba66a
Pulkit Aggarwal <pulkit.cloud@gmail.com>
2017-07-23 17:04:48 +0530

    ADDED: Electron's default window Frame.

--- main.js
+++ main.js
@@ -15,10 +15,7 @@ function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
     width: 800, 
-    height: 600,
-    frame: true,
-    titleBarStyle: 'hidden-inset', // macOS only
-    frame: process.platform === 'darwin',
+    height: 600
   })
 
   // and load the index.html of the app.
